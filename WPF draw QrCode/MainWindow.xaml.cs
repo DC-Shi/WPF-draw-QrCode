35899bdad0b75bf449048380c1f0a3181f6dcfaa
dc-shi <daochen.shi@gmail.com>
2016-11-23 12:10:18 +0800

    Workaround for issue #2 , added a try-catch block

--- WPF draw QrCode/MainWindow.xaml.cs
+++ WPF draw QrCode/MainWindow.xaml.cs
@@ -40,33 +40,42 @@ namespace WPF_draw_QrCode
         /// <param name="e">Parameter not used.</param>
         private void ClipboardNotification_ClipboardUpdate(object sender, EventArgs e)
         {
-            var text = Clipboard.GetText();
-            /// If the text is too long, we ignore it.
-            /// Currently, URL cannot be longer than 2K,
-            /// hence 3000 threshold should be OK.
-            /// Changed threshold to 2060.
-            /// Since I found a 2423 long message would not show the pic.
-            /// And URL is limited to 2048.
-            ///   * http://en.wikipedia.org/wiki/QR_code
-            ///   Maximum character storage capacity (40-L)
-            ///   Numeric only      Max. 7,089 characters(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-            ///   Alphanumeric      Max. 4,296 characters(0–9, A–Z[upper -case only], space, $, %, *, +, -, ., /, :)
-            ///   Binary / byte     Max. 2,953 characters(8 - bit bytes)(23624 bits)
-            ///   Kanji / Kana      Max. 1,817 characters
-            ///  http://www.qrcode.com/en/about/version.html
-            ///  has another detailed max chars.
-            ///  Version 40(177x177) with H can have upto 1852 Alphanumeric
-            ///  with M can have upto 2331 Bytes.
-            ///  This is confirmed by trying 2331&2332 length text.
-            ///  Work for 2331 but not for 2332.
-            ///  So setting 2100 is OK.
-            if (text.Length > 2100) return;
-            /// If no text presented, ignore it.
-            if (text.Length < 1) return;
-            /// Change textbox if necessary.
-            if (!textBoxSrc.Text.Equals(text))
+            try {
+                /// "OpenClipboard Failed (Exception from HRESULT: 0x800401D0 (CLIPBRD_E_CANT_OPEN))"
+                /// The above error might occur when executing next statement.
+                /// So catch this exception and handle it by showing message in textbox.
+                var text = Clipboard.GetText();
+                /// If the text is too long, we ignore it.
+                /// Currently, URL cannot be longer than 2K,
+                /// hence 3000 threshold should be OK.
+                /// Changed threshold to 2060.
+                /// Since I found a 2423 long message would not show the pic.
+                /// And URL is limited to 2048.
+                ///   * http://en.wikipedia.org/wiki/QR_code
+                ///   Maximum character storage capacity (40-L)
+                ///   Numeric only      Max. 7,089 characters(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+                ///   Alphanumeric      Max. 4,296 characters(0–9, A–Z[upper -case only], space, $, %, *, +, -, ., /, :)
+                ///   Binary / byte     Max. 2,953 characters(8 - bit bytes)(23624 bits)
+                ///   Kanji / Kana      Max. 1,817 characters
+                ///  http://www.qrcode.com/en/about/version.html
+                ///  has another detailed max chars.
+                ///  Version 40(177x177) with H can have upto 1852 Alphanumeric
+                ///  with M can have upto 2331 Bytes.
+                ///  This is confirmed by trying 2331&2332 length text.
+                ///  Work for 2331 but not for 2332.
+                ///  So setting 2100 is OK.
+                if (text.Length > 2100) return;
+                /// If no text presented, ignore it.
+                if (text.Length < 1) return;
+                /// Change textbox if necessary.
+                if (!textBoxSrc.Text.Equals(text))
+                {
+                    textBoxSrc.Text = text;
+                }
+            }
+            catch(System.Runtime.InteropServices.COMException come)
             {
-                textBoxSrc.Text = text;
+                textBoxSrc.Text = come.Message;
             }
         }
 
